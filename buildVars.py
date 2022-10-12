9f70adb8af32b290d0a82a635677e4e336be095e
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-11 22:45:32 -0700

    BuidVars: add some fields from NV Access add-on datastore. Re #22.
    NV Access add-on store uses JSON to represent add-on release data. Therefore add add-on license, license URL, and source (repository) URL, used when generating add-on store JSON file (there are other fields such as publisher and hash but they must be defined after JSON is created).

--- buildVars.py
+++ buildVars.py
@@ -31,6 +31,8 @@ It can span multiple lines."""),
 	"addon_author": "name <name@domain.com>",
 	# URL for the add-on documentation support
 	"addon_url": None,
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": None,
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
@@ -41,6 +43,10 @@ It can span multiple lines."""),
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": None,
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": None,
 }
 
 # Define the python files that are the sources of your add-on.
