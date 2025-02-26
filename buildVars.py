cb03ab8915d29fdf1b18f3a2320045485f6d4b5b
Joseph Lee <joseph.lee22590@gmail.com>
2025-02-26 06:28:59 -0700

    BuildVars: update minimum and last tesed version examples based on add-on store metadata (store minimum version is 2019.1.0)

--- buildVars.py
+++ buildVars.py
@@ -35,9 +35,9 @@ It can span multiple lines."""),
 	"addon_sourceURL": None,
 	# Documentation file name
 	"addon_docFileName": "readme.html",
-	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": None,
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion": None,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
